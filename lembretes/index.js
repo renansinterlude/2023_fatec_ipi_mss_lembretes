2cc8a3b7cbad9a6d30ac9971fccf98fb256f3399
Aluno <aluno@IPIRANGA.FATEC>
2023-09-20 17:08:51 -0300

    GET e POST lembretes

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,3 +1,43 @@
 require('dotenv').config()
+// usamos para definir endpoints
+// para receber requisições http
 const express = require('express')
+// usamos para enviar requisições HTTP
 const axios = require ('axios')
+
+const app = express();
+
+//middleware (funcao intermediária)
+// função pra converter json pra javascript
+app.use(express.json())
+
+/*
+  {
+    "1": {"id": "1", "texto": "Fazer cafe"},
+    "2": {"id": "1", "texto": "Fazer cafe"}
+  }
+*/
+
+const lembretes = {}
+let id = 1
+
+//GET /lembretes
+app.get('/lembretes', (req, res) => {
+    res.send(lembretes)
+})
+
+// POST /lembretes {texto: "Fazer cafe"}
+app.post('/lembretes', (req, res) => {
+    const texto = req.body.texto
+    const lembrete = {id, texto}
+    lembretes[id] = lembrete
+    // lembretes[id] = {id: texto:}
+    id++;
+    // HTTP 201 Created
+    res.status(201).send(lembretes)
+})
+
+app.listen(
+    process.env.PORT,
+    () => console.log(`Lembretes: ${process.env.PORT}`)
+)
